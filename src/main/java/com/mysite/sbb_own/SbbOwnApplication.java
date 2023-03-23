3777583b2d2c9c7b6d152904550d6efeae97a9e2
sohhhyeeun <syeankm12076@gmail.com>
2023-03-23 17:10:47 +0900

    test

---
+++ src/main/java/com/mysite/sbb_own/SbbOwnApplication.java
@@ -0,0 +1,13 @@
+package com.mysite.sbb_own;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbbOwnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbbOwnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mysite/sbb_own/SbbOwnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysite.sbb_own;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbbOwnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
